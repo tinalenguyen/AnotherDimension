b1163a15502b034a6bebc3b17f14fc271e7068dc
Tina Nguyen <thetinalenguyen@gmail.com>
2020-10-25 02:03:37 -0400

    Second method works

--- ArrayOps.java
+++ ArrayOps.java
@@ -2,7 +2,9 @@ public class ArrayOps {
 
   public static int sum(int[] arr){
 //takes array of int and returns a sum of the individual values in the array
-
+    if (arr.length == 0){
+      return 0;
+    }
     int sum = 0;
 
     for (int i = 0; i < arr.length ; i++){
@@ -11,7 +13,17 @@ public class ArrayOps {
     return sum;
   }
 
+  public static int largest(int[] arr){
+//takes a non-empty array of int and returns the largest element of the array
+    int large = 0;
+    for (int i = 0 ; i < arr.length - 1;i++){
 
+      large = Math.max(arr[i], arr[i+1]);
+
+    }
+
+    return large;
+  }
 
 
 
