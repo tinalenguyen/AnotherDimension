9e483ab31edb5dd109a0a8b8dd9d1ad79dba31a2
Tina Nguyen <thetinalenguyen@gmail.com>
2020-10-25 01:50:27 -0400

    I did the first method

---
+++ ArrayOps.java
@@ -0,0 +1,19 @@
+public class ArrayOps {
+
+  public static int sum(int[] arr){
+//takes array of int and returns a sum of the individual values in the array
+
+    int sum = 0;
+
+    for (int i = 0; i < arr.length ; i++){
+      sum += arr[i];
+    }
+    return sum;
+  }
+
+
+
+
+
+
+}
